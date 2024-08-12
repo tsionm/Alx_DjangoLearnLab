52edaf9ef8eb2a8503e2ebf5c7c27a7cd4891135
tsionmehari <ttmeharid@gmail.com>
2024-08-12 15:33:13 -0400

    Django Views and URL Configuration

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),path('relationship/', include('relationship_app.urls')),  # Include the app's URLs
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+# Class-based view to display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+
+
+
+
+
+
 
 # Create your views here.
