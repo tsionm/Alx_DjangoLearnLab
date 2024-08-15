8f238983eeb69cd7aa6158a4c6e5f83d31661360
tsionmehari <ttmeharid@gmail.com>
2024-08-15 10:50:55 -0400

    some change

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,8 +10,11 @@ def list_books(request):
 # Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/ibrary_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # Correct template path
     context_object_name = 'library'
+
+
+
 
 
 
