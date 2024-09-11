08befda3efee640c3217d2cedfac66b9cfc07c55
tsionmehari <ttmeharid@gmail.com>
2024-09-11 19:53:48 -0400

    minor change

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,12 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include  # Import include here
 from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-)
+PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView)
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
@@ -17,9 +12,10 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
+
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Ensure this line is present
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
