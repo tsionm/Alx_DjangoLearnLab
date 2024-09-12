7a071d977e0b4e5ca41a10ad7a13ecbac683ea36
tsionmehari <ttmeharid@gmail.com>
2024-09-11 20:57:56 -0400

    minor change

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
-from .models import Comment
+from .models import Post, Comment
 from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
@@ -20,10 +19,12 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
-# class PostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ['title', 'content']
+class PostForm(forms.ModelForm):
+    tags = forms.CharField(widget=TagWidget(), required=False)
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
 
 
 class CommentForm(forms.ModelForm):
@@ -31,9 +32,9 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['content']
 
-class PostForm(forms.ModelForm):
-    tags = forms.CharField(widget=TagWidget(), required=False)
+# class PostForm(forms.ModelForm):
+#     tags = forms.CharField(widget=TagWidget(), required=False)
 
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']  # Include tags in fields
+#     class Meta:
+#         model = Post
+#         fields = ['title', 'content', 'tags']  # Include tags in fields
