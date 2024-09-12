970b12717f5535e10e8826d540a0495ad8cf75bc
tsionmehari <ttmeharid@gmail.com>
2024-09-11 21:03:45 -0400

    minor changes

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
-from taggit.forms import TagWidget
+from taggit.forms import TagField
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -10,7 +11,6 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(required=True)
@@ -20,21 +20,47 @@ class UserUpdateForm(forms.ModelForm):
         fields = ['username', 'email']
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(widget=TagWidget(), required=False)
+    tags = TagField(required=False)  # Updated to use TagField
 
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
 
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+# class CustomUserCreationForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
+
+
+# class UserUpdateForm(forms.ModelForm):
+#     email = forms.EmailField(required=True)
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email']
 
 # class PostForm(forms.ModelForm):
-#     tags = forms.CharField(widget=TagWidget(), required=False)
+#     tags = TagField(required=False) 
 
 #     class Meta:
 #         model = Post
-#         fields = ['title', 'content', 'tags']  # Include tags in fields
+#         fields = ['title', 'content', 'tags']
+
+
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = ['content']
+
+# # class PostForm(forms.ModelForm):
+# #     tags = forms.CharField(widget=TagWidget(), required=False)
+
+# #     class Meta:
+# #         model = Post
+# #         fields = ['title', 'content', 'tags']  # Include tags in fields
