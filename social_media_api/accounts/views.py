6749284481f00db1a24d02768db6911b5a1ab8f8
tsionmehari <ttmeharid@gmail.com>
2024-09-26 21:51:34 -0400

    minor change

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -19,6 +19,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from .models import User
+from rest_framework.permissions import IsAuthenticated
 
 
 
